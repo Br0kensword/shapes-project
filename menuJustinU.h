e7f9d226a990f4db3a6236d5a7a8796822b813f6
Justin U <longislandhustler@gmail.com>
2014-03-02 18:42:15 -0800

    menuJustinU.h
    Header file for menu

---
+++ menuJustinU.h
@@ -0,0 +1,17 @@
+/*
+*  Program Name: menuJustinU.h
+*  Written By: Justin Unverricht
+*  Date: 11/26/13
+*  Part of Project: Lab 6 Ex 1
+*/
+
+#ifndef MENUJUSTINU_H
+#define MENUJUSTINU_H
+
+#include "RectangleJustinU.h"
+#include "CircleJustinU.h"
+
+void menu(void);
+void printMenu(void);
+
+#endif
